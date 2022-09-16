12743afcbfe210c65590bb233f66ef341e66073e
Richjerk <49521848+Richjerk@users.noreply.github.com>
2022-09-16 13:05:58 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>OBD II !</h1>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-kf8wnk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-kf8wnk)
